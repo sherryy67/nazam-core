fe594b483d1aad8c463b3041d54e5f62502ef0a5
zohaib asghar <sheralii10711@gmail.com>
2025-11-17 19:25:47 +0500

    shh

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api', routes);
 
 // Root route
 app.get('/', (req, res) => {
-  sendSuccess(res, 200, 'Welcome to Nazam Core API', {
+  sendSuccess(res, 200, 'Welcome to Nazam Core API UAE REGION', {
     version: '1.0.0',
     documentation: '/api-docs',
     health: '/api/health',
